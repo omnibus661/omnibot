c180608985a8ed3bd32e129efabcbea174bd91e1
omnibus661 <41523944+omnibus661@users.noreply.github.com>
2019-01-31 09:43:08 +0100

    file import working

--- omni.py
+++ omni.py
@@ -1,8 +1,19 @@
-from tkinter import filedialog
+from tkinter import filedialog 
 from tkinter import *
 from shutil import copy2
+import time
+import os
+
  
 root = Tk()
-root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("HTML Files","*.html"),("all files","*.*")))
-print ("Selected file " + root.filename)
-copy2(root.filename, '/')
+root.withdraw()
+root.source = filedialog.askopenfilename(initialdir = "/",title = "Select source file",filetypes = (("html files",".html"),("all files",".*"))) 
+print ("Selected file " + root.source)
+time.sleep(1)
+root.target = filedialog.askdirectory(initialdir = "/",title = "Select target directory")
+print ("Selected directory " + root.target)
+copy2(root.source, root.target, follow_symlinks=True)
+print ("Copied file to selected directory.")
+time.sleep(1)
+
+#os.rename(root.)
