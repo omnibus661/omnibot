c7e7faf226694f28a83b183926079d739d80e329
omnibus661 <41523944+omnibus661@users.noreply.github.com>
2019-02-07 16:04:46 +0100

    type selection working (in progress)

--- omni.py
+++ omni.py
@@ -1,4 +1,4 @@
-from tkinter import filedialog 
+from tkinter import filedialog
 from tkinter import *
 from shutil import copy2
 from shutil import move
@@ -15,13 +15,14 @@ init(convert=True)
 #Select Source file
 root = Tk()
 root.withdraw()
-root.source = filedialog.askopenfilename(initialdir = "/",title = "Select source file",filetypes = (("html files",".html"),("all files",".*"))) 
+root.source = filedialog.askopenfilename(
+    initialdir="/", title="Select source file", filetypes=(("html files", ".html"), ("all files", ".*")))
 
-if root.source == "": 
+if root.source == "":
 
     print(Fore.RED + "No file selected. Aborting...")
 else:
-    print (Fore.GREEN + "Selected file " + root.source)
+    print(Fore.GREEN + "Selected file " + root.source)
     time.sleep(1)
 
     #Get absolute path to import directory
@@ -31,7 +32,8 @@ else:
 
     #Copy file to import directory
     copy2(root.source, import_path, follow_symlinks=True)
-    print (Fore.GREEN + "Copied file to directory.")
+    print(Fore.GREEN + "Copied file to directory.")
+
     time.sleep(1)
 
     #Rename file
@@ -42,63 +44,114 @@ else:
     importfile = rel_path + '\import.html'
 
     #Read import file and copy to export file
-    print ("Reading...")
+    print("Reading...")
 
     count = 0
 
     #Write to file
     with open("import.html") as i:
-        with open("export.html","w") as o:
+        with open("export.html", "w") as o:
             for x in i:
                 if "<div " in x:
                     line = x.split()
                     valid = False
                     validid = False
-                    
+                    validsel = False
+                    validconfirm = False
+                    selectedType = 0
+
                     while valid != True:
                         print("\n \n")
-                        
 
-                        confirmadd = input(Fore.WHITE + "Add " + Fore.CYAN + "itemscope " + Fore.WHITE + "to " + Fore.GREEN + x.replace("\n","") + Fore.WHITE + " ? (y/n)")
+                        confirmadd = input(Fore.WHITE + "Add " + Fore.CYAN + "itemscope " + Fore.WHITE +
+                                           "to " + Fore.GREEN + x.replace("\n", "") + Fore.WHITE + " ? (y/n)")
                         if confirmadd.lower() == "y" or confirmadd.lower() == "n":
                             valid = True
 
                             if confirmadd.lower() == "y":
                                 print("\n")
-                                
+
                                 while validid != True:
                                     print("\n")
-                                    idselect = input(Fore.WHITE + "Which " +Fore.CYAN + "itemscope" + Fore.WHITE + "?")
+                                    idselect = input(
+                                        Fore.WHITE + "Which " + Fore.CYAN + "itemscope" + Fore.WHITE + "?")
 
                                     if idselect.isdigit():
 
-                                    #ID Check goes here
+                                        selectedType = idselect
 
-
-                                        pass
                                         validid = True
 
+                                        # Add 1
+                                        if selectedType == "1":
+                                            while validsel != True:
+                                                print("\n")
+                                                confirsel = input(Fore.WHITE + "Add " + Fore.CYAN + "CreativeWork" + Fore.WHITE + "? (y/n)")
+
+                                                if confirsel.lower() == "y" or confirsel.lower() == "n":
+                                                    print("\n")
+                                                    if confirsel == "y":
+                                                        print("Added.")
+
+                                                    elif confirsel == "n":
+                                                        pass
+
+                                                    validsel = True
+                                                else:
+                                                    print(Fore.RED + "Invalid input.")
+                                                    validsel = False
+
+                                        
+                                        # Add 2
+                                        if selectedType == "2":
+                                            while validsel != True:
+                                                print("\n")
+                                                confirsel = input(Fore.WHITE + "Add " + Fore.CYAN + "Article" + Fore.WHITE + "? (y/n)")
+
+                                                if confirsel.lower() == "y" or confirsel.lower() == "n":
+                                                    print("\n")
+                                                    if confirsel == "y":
+                                                        print("Added.")
+
+                                                    elif confirsel == "n":
+                                                        pass
+
+                                                    validsel = True
+                                                else:
+                                                    print(Fore.RED + "Invalid input.")
+                                                    validsel = False
+
+                                        # Add 3
+                                        if selectedType == "2":
+                                            while validsel != True:
+                                                print("\n")
+                                                confirsel = input(Fore.WHITE + "Add " + Fore.CYAN + "NewsArticle" + Fore.WHITE + "? (y/n)")
+
+                                                if confirsel.lower() == "y" or confirsel.lower() == "n":
+                                                    print("\n")
+                                                    if confirsel == "y":
+                                                        print("Added.")
+
+                                                    elif confirsel == "n":
+                                                        pass
+
+                                                    validsel = True
+                                                else:
+                                                    print(Fore.RED + "Invalid input.")
+                                                    validsel = False
+
+                                        else:
+                                            pass
+                                            #print(Fore.RED + "This should never happen.")
                                     else:
                                         print(Fore.RED + "Invalid input.")
                                         validid = False
-                            
                             else:
                                 pass
-
-                        
-
                         else:
                             print(Fore.RED + "Invalid input.") 
                             valid = False
 
-                                
-                        
-
-
-
-
-
-                    
                 count +=1
                 o.write(x)
 
