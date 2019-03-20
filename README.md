4abb7a75d3d2706927a9f13f8d035dbf045f3cbc
omnibus661 <41523944+omnibus661@users.noreply.github.com>
2019-03-20 16:18:22 +0100

    reworking selection system

--- README.md
+++ README.md
@@ -7,7 +7,3 @@ Requires at least Python 3.6
 
 
 To start use omni.bat
-
-Use typelist.txt as reference sheet
-
-use_me.html is an example file to import
--- omni.py
+++ omni.py
@@ -7,7 +7,9 @@ from shutil import copy2, move
 from time import gmtime, strftime
 from tkinter import *
 from tkinter import filedialog
+from tkinter import ttk
 import ctypes
+
 
 from colorama import Back, Fore, Style, init
 
@@ -18,11 +20,18 @@ rel_path = '\import'
 
         
 import_path = dir_path + rel_path
-
+ 
 rel_error_path = '\error_log'
 abs_error_path = dir_path + rel_error_path
 
 selection = ""
+
+    
+value1 = ""
+value = ""
+
+
+imported = False
 
 #Init
 
@@ -32,49 +41,116 @@ class GUI:
         master.title("Omnibot")
         
 
-        infotext = "Waiting for import..."
+        infotext = ""
         
-
+        
         #Widgets
+        self.searchterm = StringVar()
+
+
         self.label = Label(master, text=infotext)
-        self.import_button = Button(master, text = "Import File", command=self.imp)
-
-        self.log = Listbox(master, height = 4, width = 40)
-
-        self.close_button = Button(master, text = "Exit", command=self.quit)
-        self.text_import = Listbox(master, height = 30, width = 100)
-        self.select_button = Button(master,text = "Select", command = self.select)
+        self.import_button = Button(master, text = "Import File", command=self.imp, state = NORMAL)
+        self.log = Listbox(master, height = 4, width = 43)
+        #self.close_button = Button(master, text = "Exit", command=self.quit)
+        self.text_import = Listbox(master, selectmode = SINGLE, height = 30, width = 100, exportselection = 0)
+        self.select_button = Button(master,text = "Select", command = self.select, state = DISABLED)
         self.text_import_scroll = Scrollbar(master,command = self.text_import.yview)
-        self.additemscope_button = Button(master, text = "Add Itemtype", command = self.additemtype)
+        self.additemscope_button = Button(master, text = "Add Itemtype", command = self.additemtype, state = DISABLED)
         self.logscroll = Scrollbar(master,command = self.log.yview)
-
-        self.up_button = Button(master, text = "Identify <div> elements", command = self.ident)
-        self.down_button = Button(master, text = "▼", command = self.down)
+        self.itemprops = Listbox(master, selectmode = SINGLE, height = 15, width = 50, exportselection = 0)
+        self.itemscroll = Scrollbar(master,command = self.itemprops.yview)
+        self.ident_button = Button(master, text = "Identify <div> elements", command = self.ident, state = DISABLED)
+        #self.searchbar = Entry(master,textvariable = self.searchterm, width = 35)
+        #self.searchresults = Listbox(master, selectmode = SINGLE, height = 3, width = 50, exportselection = 0)
+        #self.searchresults_scroll = Scrollbar(master, command = self.searchresults.yview) 
 
         # Widget Placement
+        self.itemprops.place(x = 650, y = 40)
         self.label.place(x = 10,y = 500)
         self.import_button.place(x = 10, y = 570)
-        self.select_button.place(x = 80, y = 570)
-        self.close_button.place(x = 960, y = 570)
+        self.select_button.place(x = 85, y = 570)
+        #self.close_button.place(x = 960, y = 570)
         self.text_import.place(x = 10, y = 10)
         self.additemscope_button.place(x = 650, y = 10)
         self.log.place(x = 10 , y = 500)
-
-        self.up_button.place(x = 650, y = 40)
-        self.down_button.place(x = 650, y = 70)
+        self.ident_button.place(x = 135, y = 570)
         self.logscroll.place(in_ = self.log, relx = 1.0, relheight = 1.0, bordermod = "outside")
-        
+        self.itemscroll.place(in_ = self.itemprops, relx = 1.0, relheight = 1.0, bordermod = "outside")
         self.text_import_scroll.place(in_= self.text_import, relx = 1.0, relheight = 1.0, bordermod = "outside")
-        #self.text_import.insert(0, "Waiting for import...")
-
-
+        #self.searchbar.place(x = 740, y = 17)
         self.text_import.configure(yscrollcommand= self.text_import_scroll.set)
         self.log.configure(yscrollcommand = self.logscroll.set )
+        self.itemprops.configure(yscrollcommand = self.itemscroll.set)
+        #self.searchresults.place(x = 650, y = 40)
 
-   
+        #self.searchresults_scroll.place(in_ = self.searchresults, relx = 1.0, relheight = 1.0, bordermod = "outside")
+        #self.searchresults.configure(yscrollcommand = self.searchresults_scroll.set)
+
+        #Bindings
+        self.text_import.bind("<<ListboxSelect>>", self.selectLine)
+        self.itemprops.bind("<<ListboxSelect>>", self.selectProp)
+
+
+    def selectLine(self, event):
+        widget = event.widget
+        selection1 = widget.curselection()
+        global value1
+        value1 = widget.get(selection1[0])
+        return value1
+        print ("selection: " + value1.replace("\n", "") + " at index " + str(selection1[0]))
+        self.log.insert(END, "selection: " + value1.replace("\n", "") + " at index " + str(selection1[0]))
+        self.log.itemconfig(END, {'fg': '#17d637'})
+
+        
+
+    def selectProp(self, event):
+        widget = event.widget
+        selection=widget.curselection()
+        global value
+        value = widget.get(selection[0])
+        return value
+        print ("selection: " + value.replace("\n", "") + " at index " + str(selection[0]))
+        self.log.insert(END, "selection: " + value.replace("\n", "") + " at index " + str(selection[0]))
+        self.log.itemconfig(END, {'fg': '#17d637'})
+        
+
+    def additemtype(self):
+        if value1 != "":
+            if value != "":
+                #print("stuff goes here")
+                if "<div " not in value1:
+                    print("<div> element not recognized")
+        else:
+            print('Selections cannot be ""')
+
+            
+        
+        
 
     def quit(self):
         sys.exit()
+
+    # def search_itemprops(self):
+    #     search_term = self.searchterm.get()
+    #     print(search_term)
+    #     with open("typelist.txt") as typelist:
+    #         types = typelist.readlines()
+
+    #         for x in types:
+    #             ln = x
+    #             ind,tmp  = ln.split('#')
+
+
+    #     if search_term != "":
+    #         try:
+    #             index = self.itemprops.get(0,"END").index()
+    #         except ValueError:
+    #             pass
+
+
+
+    #     root.after(10, gui.search_itemprops)
+
 
     
 
@@ -98,7 +174,7 @@ class GUI:
             self.log.insert(END, "Please wait")
             self.log.itemconfig(END, {'fg': '#d4ff00'})
 
-
+ 
     
 
             # Rename file
@@ -121,36 +197,33 @@ class GUI:
                     for x in i:
 
                         self.text_import.insert(END, x)
+            
+            self.select_button['state'] = 'normal'
+            self.ident_button['state'] = 'normal'
+            self.import_button['state'] = 'disabled'
+            self.additemscope_button['state'] = 'normal'
+
+
+        # import itemtypes from list                
+        os.chdir(dir_path)
+        with open("typelist.txt", "r") as typelist:
+            rln = typelist.readlines()
+
+            for x in rln:
+                ln = x
+                tmp, lnsplit = ln.split('#')
+
+                self.itemprops.insert(END, lnsplit)
+        
+        #root.after(10, gui.search_itemprops)
+        
+        #mark itemtypes
 
 
     def select(self):
         selection = gui.text_import.get(ACTIVE)
         gui.label['text'] = selection
 
-    def up(self):
-       a
-
-    def down(self): 
-        selection = gui.text_import.get(ACTIVE)
-        index = gui.text_import.get(0, "end").index(selection)
-        gui.text_import.itemconfig(index, {'bg': 'white'})
-
-        while not "<div " in selection:
-            index +=1
-            selection = gui.text_import.get(index)
-            gui.text_import.yview(index)
-            gui.text_import.activate(index)
-            gui.text_import.see(index)
-        gui.text_import.itemconfig(index, {'bg': '#5b5bff'})
-
-        if "<div" in selection:
-            a
-
-
-
-        gui.label['text'] = selection
-        
-        #root.after(1, down(self))
 
     def ident(self):
         x = 0
@@ -166,14 +239,16 @@ class GUI:
         gui.log.insert(END, "Highlighted all <div> elements")
         gui.log.itemconfig(END, {'fg': '#d4ff00'})
 
-    def additemtype(self):
-            
-        a
-    
+    def passfunction(self):
+        pass
+
 
 root = Tk()
 gui = GUI(root)
 root.geometry("1000x600")
+root.resizable(False, False)
+root.protocol('WM_DELETE_WINDOW', gui.passfunction)
+
 
 
 
